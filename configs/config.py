02b2b403459326956de3c2abbf563d4e046200b4
Fakhri Prayatna <fakhripraya.p@gmail.com>
2024-06-23 16:16:44 +0700

    tidy up

--- configs/config.py
+++ configs/config.py
@@ -4,6 +4,7 @@
 import os
 
 INSECURE_PORT = str(os.getenv("INSECURE_PORT"))
+OPENAI_MODEL = str(os.getenv("OPENAI_MODEL"))
 OPENAI_API_KEY = str(os.getenv("OPENAI_API_KEY"))
 ADVERTISING_PIC_NUMBER = str(os.getenv("ADVERTISING_PIC_NUMBER"))
 SERVICE_PIC_NUMBER = str(os.getenv("SERVICE_PIC_NUMBER"))
--- src/infra/langchain/api.py
+++ src/infra/langchain/api.py
@@ -6,7 +6,7 @@ from src.infra.langchain.prompt_parser.prompt_parser import PromptParser
 from src.infra.weaviate.api import WeaviateAPI
 from src.interactor.interfaces.logger.logger import LoggerInterface
 from src.domain.constants import OPENAI, HUGGING_FACE
-from configs.config import ADVERTISING_PIC_NUMBER, SERVICE_PIC_NUMBER
+from configs.config import ADVERTISING_PIC_NUMBER, SERVICE_PIC_NUMBER, OPENAI_MODEL
 from src.domain.prompt_templates import chat_template, analyzer_template
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
@@ -20,7 +20,6 @@ class LangchainAPI(LangchainAPIInterface, WeaviateAPI):
     """
 
     def __init__(self, llm_type: str, api_key: str, logger: LoggerInterface) -> None: 
-        # TODO: Implement weaviate here
         self._api_key = api_key
         self._logger = logger
         self._store = {}
@@ -34,7 +33,7 @@ class LangchainAPI(LangchainAPIInterface, WeaviateAPI):
         """ 
         Create OpenAI LLM and register it as dependency
         """
-        client = ChatOpenAI(model="gpt-4", api_key=self._api_key)
+        client = ChatOpenAI(model=OPENAI_MODEL, api_key=self._api_key)
         WeaviateAPI.__init__(self, self._logger)
         
         self._client = client
@@ -43,13 +42,6 @@ class LangchainAPI(LangchainAPIInterface, WeaviateAPI):
             "analyzer_template": [analyzer_template],
             "chat_template": chat_template
         }
-        
-        # with_message_history = RunnableWithMessageHistory(
-        #     runnable,
-        #     self.get_session_history,
-        #     input_messages_key="input",
-        #     history_messages_key="history",
-        # )
 
     def create_huggingface_llm(self) -> None:
         """ 
@@ -81,7 +73,6 @@ class LangchainAPI(LangchainAPIInterface, WeaviateAPI):
         :param prompt: chat message to be analyzed.
         """
         result = self._prompt_parser.execute(prompt, self._templates)
-        print(result)
         output = self.feedback_prompt(prompt)
         
         return output
@@ -126,11 +117,4 @@ class LangchainAPI(LangchainAPIInterface, WeaviateAPI):
         command, a simple chat, etc.
         :param prompt: chat message to be analyzed.
         """
-        # messages = [{"role": "user", "content": prompt}]
-        # response = self._client.chat.completions.create(
-        #     model="gpt-4",
-        #     messages=messages,
-        #     temperature=0.1,
-        # )
-        # return response.choices[0].message.content
         return prompt
