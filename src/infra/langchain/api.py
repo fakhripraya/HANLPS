530fd5e9dcb21d03d03f56174048742ae97a985b
Fakhri Prayatna <fakhripraya.p@gmail.com>
2024-09-24 19:12:49 +0700

    change filter operand

--- src/infra/langchain/api.py
+++ src/infra/langchain/api.py
@@ -220,9 +220,9 @@ class LangchainAPI(LangchainAPIInterface, WeaviateAPI):
                 if len(filter_array["housing_price"]) > 0:
                     filters = Filter.all_of(filter_array["housing_price"])
                 if len(filter_array["building_facility"]) > 0:
-                    filters = filters | Filter.any_of(filter_array["building_facility"]) if filters else Filter.any_of(filter_array["building_facility"])
+                    filters = filters & Filter.any_of(filter_array["building_facility"]) if filters else Filter.any_of(filter_array["building_facility"])
                 if len(filter_array["building_note"]) > 0:
-                    filters = filters | Filter.any_of(filter_array["building_note"]) if filters else Filter.any_of(filter_array["building_note"])
+                    filters = filters & Filter.any_of(filter_array["building_note"]) if filters else Filter.any_of(filter_array["building_note"])
                 
                 while len(building_list) < limit:
                     self._logger.log_info("Execute query")
